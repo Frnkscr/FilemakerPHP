44e4189502c84240efdc81d3acabd11960748a52
Frnkscr <fbarreragomez@gmail.com>
2015-03-27 20:55:45 -0600

    Creo nuevo Archivo
    Creo archivo original

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PortalWebFileMaker</title>
+	<link rel="stylesheet" type="text/css" href="CSS/Index.css">
+</head>
+<body>
+	<header>
+		<h1>Menu</h1>
+		
+		<!--<nav class= "background-blue"> -->
+		<div class = "container">
+
+			<h2 href = "#"> <?php echo "Reporte Facturación"; ?>	</h2>
+			<h2>Report Timbrado Tickets</h2>
+			<h2>Reporte de Direccion Ventas Realizadas</h2>
+			
+		</div>			
+		<!--</nav> -->
+	</header>
+
+	<?php
+	echo "Hola mundo";
+	?>	
+</body>
+</html>
+
+
